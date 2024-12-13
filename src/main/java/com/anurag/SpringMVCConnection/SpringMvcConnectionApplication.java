9cca9a54511116c214c3a9904fbb0a01768c3183
Anurag Gupta <anupambalaji2019@gmail.com>
2024-12-13 14:19:46 +0300

    init commit

---
+++ src/main/java/com/anurag/SpringMVCConnection/SpringMvcConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.anurag.SpringMVCConnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcConnectionApplication.class, args);
+	}
+
+}
